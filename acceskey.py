6f9590a1ca2068e4bcc8359a015a1dd9e526fdb8
xCoruPT_daTAx <114884443+EvanCagiannos@users.noreply.github.com>
2023-03-01 22:30:12 -0500

    Update acceskey.py

--- acceskey.py
+++ acceskey.py
@@ -4,3 +4,5 @@ print(key)
 # line below states that the open function creates a file called thekey.key and opens it it write binary mode which is referd to thekey
 
 with open ("thekey.key", "wb") as thekey:
+    thekey.write(key)
+    
