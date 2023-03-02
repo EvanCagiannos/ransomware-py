18da6cc166dc9325c2103484d85e628f41ddc577
xCoruPT_daTAx <114884443+EvanCagiannos@users.noreply.github.com>
2023-03-01 22:24:14 -0500

    Create acceskey.py

---
+++ acceskey.py
@@ -0,0 +1,6 @@
+# this is the key to the incrpyted data 
+print(key)
+
+# line below states that the open function creates a file called thekey.key and opens it it write binary mode which is referd to thekey
+
+with open ("thekey.key", "wb") as thekey:
