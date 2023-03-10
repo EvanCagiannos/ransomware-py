921c9b535cd92d1bd40ae13a8e8451225e57ce0c
xCoruPT_daTAx <114884443+EvanCagiannos@users.noreply.github.com>
2023-03-09 21:46:11 -0500

    fixed errors with ChatGPT

--- acceskey.py
+++ acceskey.py
@@ -1,7 +1,8 @@
 # import the Fernet module from the cryptography library
+import os
 from cryptography.fernet import Fernet
 
-# generate a new 
+# generate a new key
 key = Fernet.generate_key()
 
 # print the key
@@ -9,8 +10,7 @@ print(key)
 
 # write the key to a file named "thekey.key" in binary mode
 with open("thekey.key", "wb") as thekey:
-    thekey.write(key)  
-cat thekey.key
+    thekey.write(key)
 
 # ------- DECRYPTION --------
 
@@ -18,21 +18,24 @@ files = []
 
 # loop through the files in the current directory
 for file in os.listdir():
-    if file == "thekey.key" == or "privatekey.key" or == "decrypt.py":
-        continue 
+    if file == "thekey.key" or file == "privatekey.key" or file == "decrypt.py":
+        continue
     if os.path.isfile(file):
         files.append(file)
 
-# print the list of files to be encrypted
+# print the list of files to be decrypted
 print(files)
 
-with open("thekey.key", "rb") as key: 
-        secretkey = key.read()
-        
-        # read the contents of the file
-        contents = thefile.read()
-        # encrypt the contents using the generated key
-        contents_decrypted = Fernet(secretkey).decrypt(contents)
-        # open the file in binary write mode
-        with open(file, "wb") as thefile:
-            thefile.write(contents_decrypted)
+with open("thekey.key", "rb") as key_file:
+    secret_key = key_file.read()
+
+    # loop through the files and decrypt each file
+    for file in files:
+        with open(file, "rb") as encrypted_file:
+            # read the contents of the file
+            contents = encrypted_file.read()
+            # decrypt the contents using the secret key
+            contents_decrypted = Fernet(secret_key).decrypt(contents)
+            # open the file in binary write mode
+            with open(file, "wb") as decrypted_file:
+                decrypted_file.write(contents_decrypted)
