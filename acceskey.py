b97971afcca1b51e9011f7423468cab1435b4de8
xCoruPT_daTAx <114884443+EvanCagiannos@users.noreply.github.com>
2023-03-09 21:14:26 -0500

    Update acceskey.py

--- acceskey.py
+++ acceskey.py
@@ -1,7 +1,7 @@
 # import the Fernet module from the cryptography library
 from cryptography.fernet import Fernet
 
-# generate a new key
+# generate a new 
 key = Fernet.generate_key()
 
 # print the key
@@ -9,4 +9,30 @@ print(key)
 
 # write the key to a file named "thekey.key" in binary mode
 with open("thekey.key", "wb") as thekey:
-    thekey.write(key)
+    thekey.write(key) 
+cat thekey.key
+
+# ------- DECRYPTION --------
+
+files = []
+
+# loop through the files in the current directory
+for file in os.listdir():
+    if file == "thekey.key" == or "privatekey.key" or == "decrypt.py"
+        continue 
+    if os.path.isfile(file):
+        files.append(file)
+
+# print the list of files to be encrypted
+print(files)
+
+with open("thekey.key", "rb") as key: 
+        secretkey = key.read()
+        
+        # read the contents of the file
+        contents = thefile.read()
+        # encrypt the contents using the generated key
+        contents_decrypted = Fernet(secretkey).decrypt(contents)
+        # open the file in binary write mode
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted)
